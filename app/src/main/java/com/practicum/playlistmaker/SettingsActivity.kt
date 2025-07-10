ef698f415cfb86df52a8b95ef4d20ef0d11615e3
valtherys <melikhovainna@gmail.com>
2025-07-10 17:33:28 +0300

    add main and settings screens and navigation

---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.practicum.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val btnBack = findViewById<TextView>(R.id.btn_back)
+
+        btnBack.setOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
+    }
+}
